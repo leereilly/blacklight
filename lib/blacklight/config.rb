d499a537ff84ada5f19949c930eeba285b8d2df0
Chris Beer <chris_beer@wgbh.org>
2011-08-09 13:35:43 -0400

    add method documentation for Blacklight::Config

--- lib/blacklight/config.rb
+++ lib/blacklight/config.rb
@@ -1,18 +1,28 @@
 module Blacklight
-  class Config < DelegateClass(Hash)
-    alias_method :_get, :[] # preserve the original #[] method
-    protected :_get # make it protected
-
-    def initialize hash 
+  ##
+  # Blacklight::Config holds the configuration for a Blacklight::Controller, including
+  # fields to display, facets to show, sort options, and search fields.
+  #
+  # ActiveSupport::OrderedOptions is documented at http://api.rubyonrails.org/classes/ActiveSupport/OrderedOptions.html
+  class Config < ActiveSupport::OrderedOptions
+    ##
+    # Initialize a new Blacklight::Config from a legacy Blacklight configuration
+    #
+    # @param [Hash] hash
+    def initialize hash = {}
       @_config = hash
       super(@_config)
       self.class.compile_methods!(keys)
     end
 
+    ##
+    # Helper method for loading a legacy blacklight configuration into the new style Blacklight::Config
     def self.from_legacy_configuration config
       Blacklight::Config.new Marshal.load(Marshal.dump(config))
     end
 
+    ## 
+    # Rather than relying on method_missing or otherwise, bake configuration accessors into the class
     def self.compile_methods! keys
       keys.reject { |m| self.class.method_defined?(m) }.each do |key|
         class_eval <<-RUBY, __FILE__, __LINE__ + 1
@@ -21,6 +31,10 @@ module Blacklight
       end
     end
 
+    ##
+    # Provide a 'deep copy' of Blacklight::Config that can be modifyed without affecting
+    # the original Blacklight::Config instance.
+    #
     def inheritable_copy
       Blacklight::Config.new Marshal.load(Marshal.dump(@_config))
     end
--- lib/blacklight/configuration.rb
+++ lib/blacklight/configuration.rb
@@ -2,18 +2,31 @@ module Blacklight
   module Configuration
     extend ActiveSupport::Concern
 
+    # Add a blacklight_config method to the class, class instance, and as a helper method
     included do
       class_attribute :blacklight_config
       helper_method :blacklight_config
     end
 
+    ##
+    # If Blacklight::Configuration is included explicitly in a class, create a separate copy of the current
+    # configuration for use. If it is only loaded implicitly, blacklight_config is used by-reference
+    #
+    # @param [Class] base_class
     def self.included base_class
       return unless base_class.respond_to? :blacklight_config=
       if base_class.respond_to?(:blacklight_config) and base_class.blacklight_config
         base_class.blacklight_config = base_class.blacklight_config.inheritable_copy
       else
-        base_class.blacklight_config = Blacklight::Config.from_legacy_configuration(Blacklight.config)
+        base_class.blacklight_config = Blacklight::Configuration.default_configuration
       end
+    end
+
+    ##
+    # The default configuration object, by default it reads from Blacklight.config for backwards
+    # compatibility with Blacklight <= 3.1
+    def self.default_configuration
+      Blacklight::Config.from_legacy_configuration(Blacklight.config)
     end
 
   end
